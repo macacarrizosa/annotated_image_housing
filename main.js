0adf1b765203931714b825162f424dedec00e40b
Macarena <macarena.carrizosa@journalism.cuny.edu>
2020-03-10 18:31:02 -0400

    code DRY

--- main.js
+++ main.js
@@ -1,41 +1,43 @@
-$("body").on("click", ".dot:not(.active)", function (e) {
+$("body").on("click", ".dot:not(.active), .triangle-right:not(.active)", function (e) {
   e.preventDefault();
 
   const this_dot = e.target;
 
-  const $this_dot = $(this_dot).closest(".dot");
+  const $this_dot = $(this_dot).closest(".dot, .triangle-right");
 
   $this_dot.addClass("active");
 });
 
-$("body").on("click", ".dot.active .close", function (e) {
+$("body").on("click", ".dot.active .close, .triangle-right.active .close", function (e) {
   e.preventDefault();
 
   const this_dot = e.target;
 
-  const $this_dot = $(this_dot).closest(".dot");
+  console.log("hi!");
+
+  const $this_dot = $(this_dot).closest(".dot, .triangle-right");
 
   $this_dot.removeClass("active");
   });
 
   //maca test//
 
-  $("body").on("click", ".triangle-right:not(.active)", function (e) {
-    e.preventDefault();
-
-    const this_triangleright = e.target;
-
-    const $this_triangleright = $(this_triangleright).closest(".triangle-right");
-
-    $this_triangleright.addClass("active");
-  });
-
-  $("body").on("click", ".triangle-right.active .close", function (e) {
-    e.preventDefault();
-
-    const this_triangleright = e.target;
-
-    const $this_triangleright = $(this_triangleright).closest(".triangle-right");
-
-    $this_triangleright.removeClass("active");
-  });
+  // $("body").on("click", ".triangle-right:not(.active)", function (e) {
+  //   e.preventDefault();
+  //
+  //   const this_triangleright = e.target;
+  //
+  //   const $this_triangleright = $(this_triangleright).closest(".triangle-right");
+  //
+  //   $this_triangleright.addClass("active");
+  // });
+  //
+  // $("body").on("click", ".triangle-right.active .close", function (e) {
+  //   e.preventDefault();
+  //
+  //   const this_triangleright = e.target;
+  //
+  //   const $this_triangleright = $(this_triangleright).closest(".triangle-right");
+  //
+  //   $this_triangleright.removeClass("active");
+  // });
