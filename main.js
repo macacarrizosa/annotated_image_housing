d72649ebf9184797c3f97028bbd4d58fe6e6a0f8
Macarena <macarena.carrizosa@journalism.cuny.edu>
2020-03-10 15:50:37 -0400

    content

--- main.js
+++ main.js
@@ -16,4 +16,26 @@ $("body").on("click", ".dot.active .close", function (e) {
   const $this_dot = $(this_dot).closest(".dot");
 
   $this_dot.removeClass("active");
-});
+  });
+
+  //maca test//
+
+  $("body").on("click", ".triangle-right:not(.active)", function (e) {
+    e.preventDefault();
+
+    const this_triangleright = e.target;
+
+    const $this_triangleright = $(this_triangleright).closest(".triangle-right");
+
+    $this_triangleright.addClass("active");
+  });
+
+  $("body").on("click", ".triangle-right.active .close", function (e) {
+    e.preventDefault();
+
+    const this_triangleright = e.target;
+
+    const $this_triangleright = $(this_triangleright).closest(".triangle-right");
+
+    $this_triangleright.removeClass("active");
+  });
